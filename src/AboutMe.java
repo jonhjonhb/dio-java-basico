bcc49158cb3777bfb05e742345384317e89a8af0
jonhjonhb <augustosales2010@gmail.com>
2023-03-25 11:05:35 -0300

    criação de classe para pegar input do usuario

---
+++ src/AboutMe.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Seu nome é " + nome + " " + sobrenome);
+        System.out.println("Você tem " + idade + " anos");
+    }
+}
